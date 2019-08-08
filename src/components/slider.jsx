3a31850015b8298eeb58a1c1f970311db22ddea7
pepdbm7 <pepdbm7@gmail.com>
2019-08-08 20:26:00 +0200

    slider working and installed gsap

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,27 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import Kid from "../img/kid.jpg";
+import Rocks from "../img/rocks.jpg";
+import Dogs from "../img/dogs.jpg";
+import Sunset from "../img/sunset.jpg";
 
 const Slider = () => {
   const [index, setIndex] = useState(1);
 
-  const showSlides = n => {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-      slideIndex = 1;
+  const slides = [
+    {
+      quote:
+        "I love you the more in that I believe you had liked me for my own sake and for nothing else",
+      imgUrl: Kid
+    },
+    {
+      quote:
+        "But man is not made for defeat. A man can be destroyed but not defeated.",
+      imgUrl: Rocks
+    },
+    {
+      quote: "I have not failed. I've just found 10,000 ways that won't work.",
+      imgUrl: Dogs
+    },
+    {
+      quote: "Be yourself; everyone else is already taken.",
+      imgUrl: Sunset
     }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+  ];
+
+  const length = slides.length - 1;
+
+  const handleNext = () => {
+    console.log("next");
+    index === length ? setIndex(0) : setIndex(index + 1);
   };
+
+  const handlePrevious = () => {
+    console.log("previous");
+
+    index === 0 ? setIndex(length) : setIndex(index - 1);
+  };
+
+  const { quote, imgUrl } = slides[index];
+
+
+  const currentSlide = i => {
+    setIndex(i)
+  }
 
   return (
     <React.Fragment>
@@ -31,63 +56,31 @@ const Slider = () => {
         <div class="mySlides">
           <div className="slide">
             <div className="leftSide">
-              <q>
-                I love you the more in that I believe you had liked me for my
-                own sake and for nothing else
-              </q>
+              <q>{quote}</q>
             </div>
             <div className="rightSide">
-              <img src="../img/kid.jpg" alt="img1" />
-            </div>
-          </div>
-
-          <div className="slide">
-            <div className="leftSide">
-              <q>
-                But man is not made for defeat. A man can be destroyed but not
-                defeated.
-              </q>
-            </div>
-            <div className="rightSide">
-              <img src="../img/rocks.jpg" alt="img2" />
-            </div>
-          </div>
-
-          <div className="slide">
-            <div className="leftSide">
-              <q>
-                I have not failed. I've just found 10,000 ways that won't work.
-              </q>
-            </div>
-            <div className="rightSide">
-              <img src="../img/dogs.jpg" alt="img3" />
-            </div>
-          </div>
-
-          <div className="slide">
-            <div className="leftSide">
-              <q>Be yourself; everyone else is already taken.</q>
-            </div>
-            <div className="rightSide">
-              <img src="../img/sunset.jpg" alt="img4" />
+              <img src={`${imgUrl}`} alt="img1" />
             </div>
           </div>
         </div>
 
         {/* Next/prev buttons */}
-        <p class="prev" onclick="plusSlides(-1)">
+        <p class="prev" onClick={() => handlePrevious()}>
           &#10094;
         </p>
-        <p class="next" onclick="plusSlides(1)">
+        <p class="next" onClick={() => handleNext()}>
           &#10095;
         </p>
       </div>
 
       {/* Dots/bullets/indicators */}
       <div class="dot-container">
-        <span class="dot" onclick="currentSlide(1)" />
-        <span class="dot" onclick="currentSlide(2)" />
-        <span class="dot" onclick="currentSlide(3)" />
+        {slides.map((_, i) => {
+          if(i === index) {
+            return <span class="dot active" onClick={() => currentSlide(i)} />
+          }
+          return <span class="dot" onClick={() => currentSlide(i)} />
+          })}
       </div>
     </React.Fragment>
   );
